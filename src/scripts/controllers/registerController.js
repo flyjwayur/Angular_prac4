83c2dd40ea2647e78a592c0ac6980cf500177fc9
HyeSoo <flyjwayur@gmail.com>
2016-02-08 14:10:08 +0200

    Angular service

---
+++ src/scripts/controllers/registerController.js
@@ -0,0 +1,20 @@
+angular.module('myApp')
+    .controller('registerController', function ($scope, AjaxFactory) {
+
+        $scope.register = function () {
+            var data = {
+              username: $scope.uname,
+              password: $scope.pwd,
+              email: $scope.email
+            };
+            //var fd = new FormData(document.getElementById('fileForm'));
+            
+            var request = AjaxFactory.register(data);
+
+            request.then(function (response) {
+                console.log(response.data);
+            }, function (error) {
+                console.log(error.data);
+            });
+        };
+    });
---
+++ src/scripts/services/ajaxFactory.js
@@ -0,0 +1,24 @@
+angular.module('myApp')
+    .factory('AjaxFactory', function ($http, $httpParamSerializer) {
+        var urlBase = 'http://util.mw.metropolia.fi/ImageRekt/api/v2/';
+        var ajaxFunctions = {};
+
+        ajaxFunctions.uploadFile = function (args) {
+            return $http.post(urlBase + 'upload', args, {
+                transformRequest: angular.identity,
+                headers: {
+                    'Content-Type': undefined
+                }
+            });
+        };
+
+        ajaxFunctions.register = function (args) {
+            return $http.post(urlBase + 'register', $httpParamSerializer(args), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+        };
+
+        return ajaxFunctions;
+    });
